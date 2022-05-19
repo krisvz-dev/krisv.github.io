8915d8d0abaf6206d9f3c90d1f115de7d306e108
krisvz-dev <nakoidevn@gmail.com>
2022-05-19 08:35:10 -0400

    1 file modified

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,19 @@
 <script>
   let money = 0;
+  let mpc = 1;
+  let mpcCost = 25
 
   function increment() {
-    money += 1;
+    money += mpc;
+  }
+  function buyMPC() {
+    if (money >= mpcCost) {
+      money -= mpcCost;
+      mpc++;
+    }
   }
 </script>
-<button on:click={increment}>
-  Pressed {money} times
-</button>
+<button on:click={increment}> Click! </button>
+<h3>Money: {money}</h3>
+<button on:click={buyMPC}>Buy +1 Money Per Click ${mpcCost}</button>
+<h3>MPC: {mpc}</h3>
